1f5702c5231777181893bef922047733741cf7b3
Alexis Michaltsis <alexis@michaltsis.net>
2017-10-17 21:59:09 +0200

    Lint the JS

--- docs/uebung05/js/script.js
+++ docs/uebung05/js/script.js
@@ -6,26 +6,26 @@ function submit() {
     var sum = 0.0;
     var sumString = "";
 
-    sum += parseFloat(form["size"].value);
-    if (form["cheese"].checked) {
-        sum += parseFloat(form["cheese"].value);
+    sum += parseFloat(form.size.value);
+    if (form.cheese.checked) {
+        sum += parseFloat(form.cheese.value);
     }
-    if (form["salami"].checked) {
-        sum += parseFloat(form["salami"].value);
+    if (form.salami.checked) {
+        sum += parseFloat(form.salami.value);
     }
-    if (form["spinace"].checked) {
-        sum += parseFloat(form["spinace"].value);
+    if (form.spinace.checked) {
+        sum += parseFloat(form.spinace.value);
     }
-    if (form["pineapple"].checked) {
-        sum += parseFloat(form["pineapple"].value);
+    if (form.pineapple.checked) {
+        sum += parseFloat(form.pineapple.value);
     }
-    if (form["olives"].checked) {
-        sum += parseFloat(form["olives"].value);
+    if (form.olives.checked) {
+        sum += parseFloat(form.olives.value);
     }
-    if (form["garlic"].checked) {
-        sum += parseFloat(form["garlic"].value);
+    if (form.garlic.checked) {
+        sum += parseFloat(form.garlic.value);
     }
-    
+
     // Formatiere den Floatwert
     sumString = String(sum.toFixed(2));
     sumString = sumString.replace(".", ",");
