77b50757d725e86d08393b4198614d52b5959cfe
Alexis Michaltsis <alexis@michaltsis.net>
2017-10-24 17:07:23 +0200

    Improve coding style of JS

--- docs/uebung06/js/script.js
+++ docs/uebung06/js/script.js
@@ -1,60 +1,50 @@
 var form = document.getElementById("form");
-
-
 form.addEventListener("click", totalOrder, true);
+form.addEventListener("submit", submit);
 
 function totalOrder() {
     var sum = 0.0;
-    var orderString = "";
-    var totalString = "";
+    var order = "";
+    var total = "";
 
     sum += parseFloat(form.size.value);
     if (form.size.value == "5") {
-        orderString += "Pizza " + document.getElementById("size26").innerHTML + "<br>";
+        order += "Pizza " + document.getElementById("size26").innerHTML + "<br>";
     } else {
-        orderString += "Pizza " + document.getElementById("size32").innerHTML + "<br>";
+        order += "Pizza " + document.getElementById("size32").innerHTML + "<br>";
     }
 
     if (form.cheese.checked) {
         sum += parseFloat(form.cheese.value);
-        orderString += document.getElementById("cheese").innerHTML + "<br>";
+        order += document.getElementById("cheese").innerHTML + "<br>";
     }
     if (form.salami.checked) {
         sum += parseFloat(form.salami.value);
-        orderString += document.getElementById("salami").innerHTML + "<br>";
+        order += document.getElementById("salami").innerHTML + "<br>";
     }
     if (form.spinace.checked) {
         sum += parseFloat(form.spinace.value);
-        orderString += document.getElementById("spinace").innerHTML + "<br>";
+        order += document.getElementById("spinace").innerHTML + "<br>";
     }
     if (form.pineapple.checked) {
         sum += parseFloat(form.pineapple.value);
-        orderString += document.getElementById("pineapple").innerHTML + "<br>";
+        order += document.getElementById("pineapple").innerHTML + "<br>";
     }
     if (form.olives.checked) {
         sum += parseFloat(form.olives.value);
-        orderString += document.getElementById("olives").innerHTML + "<br>";
+        order += document.getElementById("olives").innerHTML + "<br>";
     }
     if (form.garlic.checked) {
         sum += parseFloat(form.garlic.value);
-        orderString += document.getElementById("garlic").innerHTML + "<br>";
+        order += document.getElementById("garlic").innerHTML + "<br>";
     }
 
-    totalString = String(sum.toFixed(2));
-    totalString = totalString.replace(".", ",");
+    total = String(sum.toFixed(2));
+    total = total.replace(".", ",");
     
-    document.getElementById("total").innerHTML = totalString + " EUR";
-    document.getElementById("order").innerHTML = orderString;
+    document.getElementById("total").innerHTML = total + " EUR";
+    document.getElementById("order").innerHTML = order;
 }
-
-form.addEventListener("change", matchAddressInput, true);
-
-function matchAddressInput() {
-    console.log("a change happened");
-}
-
-
-form.addEventListener("submit", submit);
 
 function submit() {
     alert(
